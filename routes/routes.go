3ff63b4d893a28ae758a5f602202f9946db1bd20
ADEEL FAROOQ <60884333+adeel-farooq@users.noreply.github.com>
2026-01-25 21:48:31 +0500

    business

--- routes/routes.go
+++ routes/routes.go
@@ -12,8 +12,9 @@ func RegisterRoutes(api *gin.RouterGroup) {
 	v1.RegisterAuthRoutes(auth)
 
 	// Admin routes
-	admin := api.Group("/v1/adminrole")
-	v1.RegisterAdminRoutes(admin)
-	v1.RegisterDashboardRoutes(admin)
-	v1.RegisterTransferRoutes(admin)
+	adminrole := api.Group("/v1/adminrole")
+	v1.RegisterAdminRoutes(adminrole)
+	v1.RegisterDashboardRoutes(adminrole)
+	v1.RegisterBusinessRoutes(adminrole)
+
 }
